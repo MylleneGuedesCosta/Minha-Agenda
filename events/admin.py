6d7180afd26b44269c607de8911d23218932f162
MylleneGuedes <marciamyllene@gmail.com>
2018-09-13 19:43:45 -0300

    criando meu primeiro projeto

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("date", "event", "priority")
+    list_display_links = ("event",)
+    list_filter = ("date", "priority")
+    list_editable = ("priority",)
+    search_fields = ("event", "date")
--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils import timezone
+from libgravatar import Gravatar
 
-# Create your models here.
 
 class Event(models.Model):
     priorities_list = (
@@ -11,13 +12,23 @@ class Event(models.Model):
         ('4', 'Ultra Mrga Hiper Urgente')
     )
 
-
-
     date = models.DateField()
     event = models.CharField(max_length = 100)
     priority = models.CharField(max_length=1, choices=priorities_list)
 
-
-    def __str__(self:)
+    def __str__(self):
         return self.event
 
+
+class Coment(models.Model):
+    """Comentários efetuados em um determinado evento."""
+    author = models.CharField(max_length=80)
+    email = models.EmailField()
+    text = models.CharField(max_length=160)
+    commented = models.DateTimeField(default=timezone.now)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='comment_event')
+
+    """Retorna a partir do endereço de email, um avatar configurado no Gravatar"""
+    def avatar(self):
+        g = Gravatar(self.email)
+        return g.get_image(default='identicon')
